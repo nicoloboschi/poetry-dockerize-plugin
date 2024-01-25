3cc3d6e67e1543668e9c2f9bd870323b94416381
Nicolò Boschi <boschi1997@gmail.com>
2024-01-25 23:28:15 +0100

    test

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -14,7 +14,7 @@ def test_parse() -> None:
     print(content)
     assert content == """
 FROM python:3.11-slim-buster as builder
-RUN pip install poetry==1.4.2
+RUN pip install poetry==1.7.1
 
 ENV POETRY_NO_INTERACTION=1
 ENV POETRY_VIRTUALENVS_IN_PROJECT=1
